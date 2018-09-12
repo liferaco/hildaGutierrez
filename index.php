83941e7d9d37c095df6f13a57762ba77f54aacf4
liferaco <liferaco9625@gmail.com>
2018-09-12 14:59:24 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Hilda Gutiérrez</title>
+    <link rel="shortcut icon" href="img/icono.ico">
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="vendor/devicons/css/devicons.min.css" rel="stylesheet">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/resume.min.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <img src="img/logoCD.png">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+
+        <span class="d-block d-lg-none">Candidata a la Cámara de Representantes</span>
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.png" alt="">
+        </span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#votacion">Votación</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">Información</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#experience">Experiencia</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#education">Educación</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#awards">Historial</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container-fluid p-0">
+
+        <section class="resume-section p-3 p-lg-5 d-flex d-column" id="votacion">
+            <div class="my-auto">
+                    <video src="video/HILDA%20MARQUE%20ASÍ.mp4" type="video/mp4" autoplay controls width="1000"></video>
+                <img src="img/tarjeton2.jpg" width="1000" height="800">
+            </div>
+        </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+        <div class="my-auto">
+          <h1 class="mb-0">Hilda
+            <span class="text-primary">Gutiérrez</span>
+              <img src="img/FOTO%20HILDA%20CON%20URIBE.png" width="150" height="100" align="center">
+          </h1>
+          <div class="subheading mb-5">Candidata a la Cámara de Representantes – Cundinamarca Centro Democrático ·
+            <a href="mailto:hildagutierrezcd@gmail.com">hildagutierrezcd@gmail.com</a>
+          </div>
+          <p class="mb-5">Nació en el municipio de la Mesa. Está casada con el Ingeniero Civil Carlos Lara con quien tiene dos hijos: Carlos y Juanita. Es Contadora Pública de la Universidad La Gran Colombia; especialista en Derecho Público y Administrativo de la Universidad Autónoma, y tiene una maestría en estudios políticos de la Pontificia Universidad Javeriana. Además, adelantó estudios profesionales en Alta Dirección en Gestión y Liderazgo Estratégico y en Juntas Directivas de la Universidad de los Andes.</p>
+          <ul class="list-inline list-social-icons mb-0">
+            <li class="list-inline-item">
+              <a href="http://www.facebook.com/share.php?u=http://www.centrodemocratico.com/?q=articulo/hilda-gutierrez&title=">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://twitter.com/intent/tweet?text=Hilda%20Guti%C3%A9rrez&url=http%3A%2F%2Fwww.centrodemocratico.com%2F%3Fq%3Darticulo%2Fhilda-gutierrez&original_referer=http%3A%2F%2Fwww.centrodemocratico.com%2F%3Fq%3Darticulo%2Fhilda-gutierrez">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
+        <div class="my-auto">
+          <h2 class="mb-5">Experiencia </h2>
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Alcaldesa de la Mesa</h3>
+              <div class="subheading mb-3">Alcaldia de La Mesa</div>
+              <p>Durante su gestión se inició la construcción y  mejoramiento instituciones educativas, promovió programas de vivienda,  se implementaron iniciativas dirigidas al bienestar de la tercera edad y se fortalecieron las escuelas de formación cultural y deportiva.  En el sector agropecuario se fortaleció la asistencia técnica para los campesinos y se implementaron programas con énfasis en el desarrollo de la mujer rural.  También  se logró la pavimentación y mejoramiento de vías rurales y de un gran porcentaje del sector urbano.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">1995 - 1997</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Secretaria de Agricultura y Desarrollo Económico</h3>
+              <div class="subheading mb-3">Gobernación de Cundinamarca</div>
+              <p>Impulsó las iniciativas empresariales del departamento en el sector agropecuario y apoyó permanentemente a los municipios para fortalecer y mantener activas las  Umata (Unidad Municipal de Asistencia Técnica Agropecuaria). Además promovió la asociatividad agropecuaria de los productores fortaleciendo el capital de trabajo y dotación de equipos. También fue asesora de la Secretaria de Competitividad y Desarrollo Económico  y Jefe de Despacho y Protocolo.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">2004</span>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
+        <div class="my-auto">
+          <h2 class="mb-5">Educación</h2>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-3">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Universidad La Gran Colombia</h3>
+              <div class="subheading mb-3">Contadora Pública</div>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-3">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Universidad Autónoma</h3>
+              <div class="subheading mb-3">Especialista en Derecho Público y Administrativo</div>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-3">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Pontificia Universidad Javeriana</h3>
+              <div class="subheading mb-3">Maestría en estudios políticos</div>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-3">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Universidad de los Andes</h3>
+              <div class="subheading mb-3">Alta Dirección en Gestión y Liderazgo Estratégico y en Juntas Directivas</div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="awards">
+        <div class="my-auto">
+          <h2 class="mb-5">Historial</h2>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              Ha ejercido cargos en el Congreso de la Republica en la Comisión Sexta Permanente Constitucional y en la Dirección General de la Corporación Autónoma Regional de Cundinamarca CAR</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              Presidenta de la Corporación de abastos CORABASTOS</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              Miembro del  Consejo Directivo de la CAR</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              Presidenta de la Junta Directiva del Instituto de Cultura y Turismo de Cundinamarca</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              Presidenta de la Junta Directiva del Instituto de Deportes</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              Presidenta de la Junta Directiva del Hospital del municipio de Viotá y de Mesitas</li>
+
+          </ul>
+        </div>
+          <div align="center">
+              <h6>© 2018 Copyright Derechos Reservados Alta Especialidad SAS</h6>
+          </div>
+      </section>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/resume.min.js"></script>
+
+  </body>
+
+</html>
